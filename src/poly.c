67b32a36143177487709a13f6e3d200bc638e3fb
Mateusz Malinowski <malina314@op.pl>
2021-04-23 18:04:15 +0200

    zaimplementowanie add, addMonos, isEqual

--- src/poly.c
+++ src/poly.c
@@ -1,5 +1,7 @@
 /** @file
-  Implementacja klasy wielomianów rzadkich wielu zmiennych
+  Implementacja klasy wielomianów rzadkich wielu zmiennych.
+  Implementacja zakłada, że jednomiany, z których składa się wielomian są
+  posortowane rosnąco po wykładniku.
 
   @authors Mateusz Malinowski
   @date 2021
@@ -10,8 +12,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define POLY(s) {.size = (s), .arr = malloc((s) * sizeof (Mono))}
 
+//#define WYPISYWANIE
+
+#ifdef WYPISYWANIE
+#include "print.h"
+#endif
+
+/**
+ * Sprawdza, czy udało się zaalokować pamięć. Jeśli nie, kończy działanie
+ * programu z kodem 1.
+ * @param[in] p : wskaźnik zwrócony przez funkcję alokującą pamięć
+ */
 #define CHECK_PTR(p)        \
     do {                    \
         if (p == NULL) {    \
@@ -19,11 +31,21 @@
         }                   \
     } while (0)
 
+/**
+ * Tworzy wielomian składający się z @p size jednomianów.
+ * @param[in] size : rozmiar tablicy jednomianów
+ * @return wielomian
+ */
+static inline Poly PolyCreate(size_t size) {
+    return (Poly) {.size = (size), .arr = malloc((size) * sizeof (Mono))};
+}
+
 void PolyDestroy(Poly *p) {
     if (!PolyIsCoeff(p)) {
         for (size_t i = 0; i < p->size; ++i) {
             MonoDestroy(&p->arr[i]);
         }
+        free(p->arr);
     }
 }
 
@@ -32,16 +54,21 @@ Poly PolyClone(const Poly *p) {
         return *p;
     }
     else {
-        Poly pNew = POLY(p->size);
-        CHECK_PTR(pNew.arr);
+        Poly newPoly = PolyCreate(p->size);
+        CHECK_PTR(newPoly.arr);
         for (size_t i = 0; i < p->size; ++i) {
-            pNew.arr[i] = MonoClone(&p->arr[i]);
+            newPoly.arr[i] = MonoClone(&p->arr[i]);
         }
-        return pNew;
+        return newPoly;
     }
 }
 
-static bool isSorted(const Poly *p) {
+/**
+ * Sprawdza, czy jednomiany wielomianu są posortowane rosnąco po wykładniku.
+ * @param[in] p : wielomian
+ * @return Czy wielomian jest posortowany?
+ */
+static bool PolyIsSorted(const Poly *p) {
     if (!PolyIsCoeff(p)) {
         for (size_t i = 1; i < p->size; ++i) {
             if (p->arr[i - 1].exp >= p->arr[i].exp) {
@@ -52,21 +79,90 @@ static bool isSorted(const Poly *p) {
     return true;
 }
 
-Poly PolyAdd(const Poly *p, const Poly *q) {
-    assert(isSorted(p) && isSorted(q));
+/**
+ * Tworzy wielomian składający się tylko z jednego jednomianu.
+ * Przejmuje jednomian na własność.
+ * @param[in] m : jednomian
+ * @return wielomian
+ */
+static Poly PolyFormMono(Mono m) {
+    Poly p = PolyCreate(1);
+    CHECK_PTR(p.arr);
+    p.arr[0] = m;
+    return p;
+}
 
-    // TODO: sprawdzić czy to działa
-    if (PolyIsCoeff(p)) {
-        Poly tmp = PolyFromCoeff(p->coeff);
+/**
+ * Zmniejsza rozmiar tablicy jednomianów wielomianu.
+ * @param[in] p : wielomian
+ * @param[in] size : nowy rozmiar
+ */
+static void PolyShrinkArray(Poly *p, size_t size) {
+    assert(!PolyIsCoeff(p) && size <= p->size);
+    p->arr = realloc(p->arr, size * sizeof (Mono));
+    CHECK_PTR(p->arr);
+    p->size = size;
+}
+
+/**
+ * Usuwa z wielomianu jednomiany o zerowym współczynniku.
+ * @param[in] p : wielomian
+ */
+static void PolyNormalize(Poly *p) {
+    if (!PolyIsCoeff(p)) {
+        size_t k = 0;
+        Mono *newArr = malloc(p->size * sizeof (Mono));
+        CHECK_PTR(newArr);
+
+        for (size_t i = 0; i < p->size; ++i) {
+            if (!PolyIsCoeff(&p->arr[i].p)) {
+                PolyNormalize(&p->arr[i].p);
+            }
+
+            if (!PolyIsZero(&p->arr[i].p)) {
+                newArr[k++] = p->arr[i];
+            }
+            else {
+                MonoDestroy(&p->arr[i]);
+            }
+        }
+
+        free(p->arr);
+        p->arr = newArr;
+
+        if (k == 0) {
+            *p = PolyZero();
+            free(newArr);
+        }
+        else if (k == 1 && p->arr[0].exp == 0 && PolyIsCoeff(&p->arr[0].p)) {
+            *p = p->arr[0].p;
+            free(newArr);
+        }
+        else {
+            PolyShrinkArray(p, k);
+        }
+    }
+}
+
+Poly PolyAdd(const Poly *p, const Poly *q) {
+    assert(PolyIsSorted(p) && PolyIsSorted(q));
+
+    Poly tmp = PolyZero();
+
+    if (PolyIsCoeff(p) && PolyIsCoeff(q)) {
+        return PolyFromCoeff(p->coeff + q->coeff);
+    }
+    else if (PolyIsCoeff(p)) {
+        tmp = PolyFormMono(MonoFromPoly(p, 0));
         p = &tmp;
     }
-    if (PolyIsCoeff(q)) {
-        Poly tmp = PolyFromCoeff(q->coeff);
+    else if (PolyIsCoeff(q)) {
+        tmp = PolyFormMono(MonoFromPoly(q, 0));
         q = &tmp;
     }
 
     size_t i = 0, j = 0, k = 0;
-    Poly res = POLY(p->size + q->size);
+    Poly res = PolyCreate(p->size + q->size);
     CHECK_PTR(res.arr);
 
     while (i < p->size && j < q->size) {
@@ -75,33 +171,44 @@ Poly PolyAdd(const Poly *p, const Poly *q) {
             res.arr[k++].p = PolyAdd(&p->arr[i++].p, &q->arr[j++].p);
         }
         else if (p->arr[i].exp < q->arr[j].exp) {
-            res.arr[k].exp = p->arr[i].exp;
-            res.arr[k++].p = PolyClone(&p->arr[i++].p);
+            res.arr[k++] = MonoClone(&p->arr[i++]);
         }
         else {
-            res.arr[k].exp = q->arr[i].exp;
-            res.arr[k++].p = PolyClone(&q->arr[i++].p);
+            res.arr[k++] = MonoClone(&q->arr[j++]);
         }
     }
 
     while (i < p->size) {
-        res.arr[k].exp = p->arr[i].exp;
-        res.arr[k++].p = PolyClone(&p->arr[i++].p);
+        res.arr[k++] = MonoClone(&p->arr[i++]);
     }
 
     while (j < q->size) {
-        res.arr[k].exp = q->arr[i].exp;
-        res.arr[k++].p = PolyClone(&q->arr[i++].p);
+        res.arr[k++] = MonoClone(&q->arr[j++]);
     }
 
     if (k < p->size + q->size) {
-        res.arr = realloc(res.arr, k * sizeof (Mono));
-        CHECK_PTR(res.arr);
+        PolyShrinkArray(&res, k);
     }
+
+#ifdef WYPISYWANIE
+    PolyPrint(&res, 0);
+#endif
+
+    PolyNormalize(&res);
+
+    PolyDestroy(&tmp);
 
     return res;
 }
 
+/**
+ * Porównuje dwa jednomiany po wykładniku.
+ * @param a : wielomian @f$a@f$
+ * @param b : wielomian @f$b@f$
+ * @return @f$1@f$ jeśli @f$a@f$ jest większy niż @f$b@f$,
+ * @f$0@f$ jeśli są równe,
+ * @f$-1@f$ jeśli @f$b@f$ jest większy niż @f$a@f$
+ */
 static int cmpMonos(const void* a, const void* b)
 {
     const Mono *arg1 = (const Mono *)a;
@@ -121,31 +228,69 @@ Poly PolyAddMonos(size_t count, const Mono monos[]) {
 
     qsort(monosArr, count, sizeof (Mono), cmpMonos);
 
-    Poly res = POLY(count);
+    Poly res = PolyCreate(count);
     CHECK_PTR(res.arr);
 
     size_t k = 0;
 
-    res.arr[k++] = MonoClone(&monosArr[0]);
+//    res.arr[k++] = MonoClone(&monosArr[0]);
+    res.arr[k++] = monosArr[0];
 
     for (size_t i = 1; i < count; ++i) {
-        if (monosArr[i].exp == monosArr[i - 1].exp) {
-            res.arr[k - 1].p = PolyAdd(&res.arr[k - 1].p, &monosArr[i].p);
+        if (monosArr[i].exp == res.arr[k - 1].exp) {
+            Poly sum = PolyAdd(&res.arr[k - 1].p, &monosArr[i].p);
+            MonoDestroy(&res.arr[k - 1]);
+            MonoDestroy(&monosArr[i]);
+            res.arr[k - 1].p = sum;
         }
         else {
-            res.arr[k++] = MonoClone(&monosArr[i++]);
+//            res.arr[k++] = MonoClone(&monosArr[i++]);
+            res.arr[k++] = monosArr[i];
         }
     }
 
     if (k < count) {
-        res.arr = realloc(res.arr, k * sizeof (Mono));
-        CHECK_PTR(res.arr);
+        PolyShrinkArray(&res, k);
     }
+
+    PolyNormalize(&res);
 
     return res;
 }
 
+/**
+ * Sprawdza równość dwóch jednomianów.
+ * @param[in] m : jednomian @f$m@f$
+ * @param[in] n : jednomian @f$n@f$
+ * @return @f$m = n@f$
+ */
+static inline bool MonoIsEq(const Mono *m, const Mono *n) {
+    return m->exp == n->exp && PolyIsEq(&m->p, &n->p);
+}
+
 //TODO:
+// sprawdzić czy na pewno wielomiany będą znormalizowane
 bool PolyIsEq(const Poly *p, const Poly *q) {
-    return true;
+    assert(PolyIsSorted(p) && PolyIsSorted(q));
+
+//    PolyNormalize((Poly *)p);
+//    PolyNormalize((Poly *)q);
+
+    if (PolyIsCoeff(p) && PolyIsCoeff(q)) {
+        return p->coeff == q->coeff;
+    }
+    else if (!PolyIsCoeff(p) && !PolyIsCoeff(q)) {
+        if (p->size != q->size) {
+            return false;
+        }
+
+        for (size_t i = 0; i < p->size; ++i) {
+            if (!MonoIsEq(&p->arr[i], &q->arr[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    return false;
 }
